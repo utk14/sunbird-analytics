fca6043d105809b7dac978f61b9a525f7be57124
amit behera <amitb@ilimi.in>
2018-10-29 15:54:19 +0530

    Issue #000 core: testing

--- platform-api/analytics-api-core/src/main/scala/org/ekstep/analytics/api/util/Test.scala
+++ platform-api/analytics-api-core/src/main/scala/org/ekstep/analytics/api/util/Test.scala
@@ -5,17 +5,18 @@ import com.datastax.driver.core.Row
 import com.datastax.driver.core.Session
 import com.datastax.driver.core.querybuilder.{ QueryBuilder => QB }
 import org.ekstep.analytics.framework.conf.AppConf
+import org.ekstep.analytics.api.Constants
 
 object Test extends App {
 
     val embeddedCassandra = AppConf.getConfig("cassandra.service.embedded.enable").toBoolean
     val host = AppConf.getConfig("spark.cassandra.connection.host")
     val port = if (embeddedCassandra) AppConf.getConfig("cassandra.service.embedded.connection.port").toInt else 9042
-    
-    println("embeddedCassandra: "+ embeddedCassandra)
-    println("host: "+ host)
-    println("port: "+ port)
-    
+
+    println("embeddedCassandra: " + embeddedCassandra)
+    println("host: " + host)
+    println("port: " + port)
+
     val cluster = {
         Cluster.builder()
             .addContactPoint(host)
@@ -23,6 +24,8 @@ object Test extends App {
             .build()
     }
     val session = cluster.connect()
+    val query = QB.select().from(Constants.PLATFORML_DB, Constants.JOB_REQUEST).allowFiltering().where(QB.eq("request_id", "requestId")).and(QB.eq("client_key", "clientKey"))
+    val resultSet = session.execute(query)
     println(session.getLoggedKeyspace)
     session.close
 }
