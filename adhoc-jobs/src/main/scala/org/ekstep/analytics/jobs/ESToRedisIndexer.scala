3f8a89f8aa455834d141a964ff77e67281cd85cc
sunil <sunils@ilimi.in>
2019-02-13 11:35:38 +0530

    Issue #SB-10366 update: spark ES to Redis Indexer job
    changes:
    - refactor

--- adhoc-jobs/src/main/scala/org/ekstep/analytics/jobs/ESToRedisIndexer.scala
+++ adhoc-jobs/src/main/scala/org/ekstep/analytics/jobs/ESToRedisIndexer.scala
@@ -1,80 +1,45 @@
 package org.ekstep.analytics.jobs
 
-import com.sksamuel.elastic4s.http.Response
-import com.sksamuel.elastic4s.http.search.SearchResponse
+import com.redislabs.provider.redis._
 import com.typesafe.config.{Config, ConfigFactory}
 import org.apache.spark.{SparkConf, SparkContext}
-import org.ekstep.analytics.util.{ElasticsearchUtil, JSONUtils, RedisUtil}
-import redis.clients.jedis.Jedis
-import redis.clients.jedis.exceptions.JedisException
-
-import scala.annotation.tailrec
+import org.ekstep.analytics.util.JSONUtils
+import org.elasticsearch.spark._
 
 object ESToRedisIndexer {
 
-    private val sc: SparkContext = new SparkContext(master="local[*]", appName = "my App", new SparkConf);
     private val config: Config = ConfigFactory.load
-    private val elasticsearchUtil = new ElasticsearchUtil(config)
-    private val redisIndex = config.getInt("redis.database.index")
-    private val redisKeyProperty: String = config.getString("redis.database.key.property.from.source")
-
-    def fetchAndIngest(): List[Boolean] = {
-
-        val index = config.getString("search.service.query.index")
-        val query = config.getString("search.service.query.jsonString")
-
-        println(s"[$index] query ===> $query")
-        require(!index.isEmpty && !query.isEmpty, "require valid inputs! index name and query cannot be empty!")
-
-        getNext(elasticsearchUtil.scrollQuery(index, query))
-
-        @tailrec
-        def getNext(response: Option[Response[SearchResponse]]): Unit = {
-            response match {
-                case Some(data) => {
-                    val _source = data.result.hits.hits
-                    val mappedData: Array[Map[String, String]] = _source.map { obj =>
-                        val source = obj.sourceAsMap
-                        Map("id" -> source(redisKeyProperty).asInstanceOf[String] , "data" -> JSONUtils.serialize(source))
-                    }
-                    ingest(mappedData)
-                    val batchResponse = elasticsearchUtil.getNextBatch
-                    batchResponse match {
-                        case Some(res) => {
-                            if (res.result.hits.hits.nonEmpty) getNext(batchResponse)
-                        }
-                        case None => println("Fetched all records!")
-                    }
-                }
-            }
-        }
-        List(true)
-    }
-
-
-
-    def ingest(data: Array[Map[String, String]]) = {
-        var connection: Jedis = null
-        try {
-            connection = RedisUtil.getConnection
-            val pipeline = connection.pipelined
-            pipeline.select(redisIndex)
-            data.foreach { obj =>
-                pipeline.set(obj("id"), obj("data"))
-            }
-            pipeline.sync
-        } catch {
-            case ex: JedisException => {
-                ex.printStackTrace()
-            }
-        } finally {
-            if (connection != null) connection.close()
-        }
-    }
 
     def main(args: Array[String]): Unit = {
-        println(s"======> Starting Redis indexing job <=======")
-        // trying out how to run this code in spark?
-        sc.parallelize(fetchAndIngest)
+
+        val index = config.getString("elasticsearch.query.index")
+        val query = config.getString("elasticsearch.query.jsonString")
+
+        println(s"[$index] query ===> $query")
+
+        require(!index.isEmpty && !query.isEmpty, "require valid inputs! index name and query cannot be empty!")
+
+        val conf = new SparkConf()
+            .setAppName("SparkEStoRedisIndexer")
+            .setMaster("local[*]")
+            // Elasticsearch settings
+            .set("es.nodes", config.getString("elasticsearch.host"))
+            .set("es.port", config.getString("elasticsearch.port"))
+            .set("es.scroll.size", config.getString("elasticsearch.scroll.size"))
+            .set("es.query", query)
+            // redis settings
+            .set("spark.redis.host", config.getString("redis.host"))
+            .set("spark.redis.port", config.getString("redis.port"))
+            .set("spark.redis.db", config.getString("redis.database.index"))
+            .set("spark.redis.max.pipeline.size", config.getString("redis.max.pipeline.size"))
+
+        val sc = new SparkContext(conf)
+        val key = config.getString("elasticsearch.index.source.key")
+
+        // todo: log details
+
+        sc.toRedisKV(
+            sc.esRDD(index).map(data => (data._2(key).asInstanceOf[String], JSONUtils.serialize(data._2)))
+        )
     }
 }
